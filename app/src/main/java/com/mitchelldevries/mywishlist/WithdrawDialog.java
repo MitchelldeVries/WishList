decd31762c834409cb3f109dac88491d4ed81cc7
Mitchell de Vries <mdvries1@live.nl>
2016-08-29 13:58:10 +0200

    Fourth Commit

--- app/src/main/java/com/mitchelldevries/mywishlist/WithdrawDialog.java
+++ app/src/main/java/com/mitchelldevries/mywishlist/WithdrawDialog.java
@@ -10,15 +10,23 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author Mitchell de Vries.
  */
 public class WithdrawDialog extends DialogFragment {
 
-    private EditText editText;
+    @BindView(R.id.dialog_edit_text)
+    EditText editText;
+    @BindView(R.id.save_action)
+    ImageButton saveButton;
+    @BindView(R.id.cancel_action)
+    ImageButton cancelButton;
 
     public static WithdrawDialog newInstance(int id) {
         Bundle args = new Bundle();
@@ -34,23 +42,21 @@ public class WithdrawDialog extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog, null);
-        editText = ButterKnife.findById(view, R.id.dialog_edit_text);
-        // Pass null as the parent view because its going in the dialog layout
-        builder.setView(view)
-                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        WishStorage storage = WishStorage.getInstance(getActivity());
-                        Wish wish = storage.findOne(getArguments().getInt("id", 0));
-                        storage.withdraw(wish, Double.parseDouble(String.valueOf(editText.getText())));
-                        getTargetFragment().onActivityResult(WishListFragment.REQUEST_WISH, Activity.RESULT_OK, null);
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        WithdrawDialog.this.getDialog().cancel();
-                    }
-                });
+        ButterKnife.bind(this, view);
+        builder.setView(view);
         return builder.create();
+    }
+
+    @OnClick(R.id.save_action)
+    void onPositiveClick() {
+        WishStorage storage = WishStorage.getInstance(getActivity());
+        Wish wish = storage.findOne(getArguments().getInt("id", 0));
+        storage.withdraw(wish, Double.parseDouble(String.valueOf(editText.getText())));
+        getTargetFragment().onActivityResult(WishListFragment.REQUEST_WISH, Activity.RESULT_OK, null);
+    }
+
+    @OnClick(R.id.save_action)
+    void onCancelClick() {
+        WithdrawDialog.this.getDialog().cancel();
     }
 }
